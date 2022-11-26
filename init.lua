a72efc12693d5a37de3ff69e6216481c369279c5
arutonee <arutonee@proton.me>
2022-11-26 10:35:59 -0500

    Added buffer deleting function from u/mlk.

--- init.lua
+++ init.lua
@@ -71,6 +71,17 @@ vim.cmd([[
   nnoremap <C-i> <C-w>-
 
   tnoremap <Esc> <C-\><C-n>
+
+  function! CleanNoNameEmptyBuffers()
+    let buffers = filter(range(1, bufnr('$')), 'buflisted(v:val) && empty(bufname(v:val)) && bufwinnr(v:val) < 0 && (getbufline(v:val, 1, "$") == [""])')
+    if !empty(buffers)
+      exe 'bd '.join(buffers, ' ')
+    else
+      echo 'No buffer deleted'
+    endif
+  endfunction
+
+  nnoremap <silent> ,C :call CleanNoNameEmptyBuffers()<CR>
 ]])
 
 -- 4-space tabs
